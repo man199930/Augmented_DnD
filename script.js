afb2144c219ee904fae9a69476788bea2ab4f685
Manu <mantrudel4@gmail.com>
2019-11-28 04:14:55 -0500

    Creation d'un meilleur UI, il faut ajuster la coloration des tuiles avec les boutons et peut-etre regarder pour devoir changer les coordonnees selon le offset

--- script.js
+++ script.js
@@ -3,65 +3,36 @@ var nbTileWidth;
 var nbTileHeight;
 var ctx= null;
 var gameMap=[];
+var canvas;
+var changementTextureMap=false;
+var btnWall;
+var btnFloor;
+var btnGoblin;
+var btnHero1;
+var btnHero2;
+var btnHero3;
+var btnHero4;
+var couleur;
 
 
-window.addEventListener("load", ()=>{
-    const canvas= document.getElementById("game");
-    ctx=canvas.getContext("2d");
 
-    var changementTextureMap=false;
 
-    function startPositionChangementTextureMap(e){
-        changementTextureMap=true;
-        //serait supposer capable de dessiner a un seul clic mais ne fonctionne pas
-        draw(e);
-    }
-    function endPositionChangementTextureMap(){
-        changementTextureMap=false;
-        ctx.beginPath();
-    }
-    function startChangemenfPasEncoreDansCanvas(){
-        changementTextureMap=true;
-    }
-    function endChangemenfPasEncoreDansCanvas(){
-        changementTextureMap=false;
-    }
-    function tuileSelectionne(e){
-        if(!changementTextureMap){
-            return;
+function creerMap(){
+    tileDimensionPixel=document.getElementById("tileDimensionPixel").value;
+    nbTileWidth=document.getElementById("nbTileWidth").value;
+    nbTileHeight=document.getElementById("nbTileHeight").value;
+    ctx=document.getElementById('game').getContext('2d');
+    ctx.canvas.width=nbTileWidth*tileDimensionPixel;
+    ctx.canvas.height=nbTileHeight*tileDimensionPixel;
+    for(var y=0; y<nbTileHeight;y++){
+        for(var x=0;x<nbTileWidth;x++){
+            gameMap[(y*nbTileWidth)+x]=1;
         }
-    }
-    function draw(e){
-        if(!changementTextureMap){
-            return;
-        }
-        //holy grail pour Canvas
-        var BB=canvas.getBoundingClientRect();
-        var tx=e.clientX-BB.left;
-        var ty=e.clientY-BB.top;
-        //fin du holy grail
-        
-        numTuile=scanMap(tx,ty);
-        gameMap[numTuile]=6;
-        drawGame();
-    }
-    canvas.addEventListener("mousedown", startPositionChangementTextureMap);
-    canvas.addEventListener("mouseup", endPositionChangementTextureMap);
-    canvas.addEventListener("mousemove", draw);
-    document.addEventListener("mousedown", startChangemenfPasEncoreDansCanvas);
-    document.addEventListener("mouseup", endChangemenfPasEncoreDansCanvas);
 
-});
-
-function scanMap(tx, ty){
-    var tuilePositionX=tx/tileDimensionPixel;
-    var tuilePositionY=ty/tileDimensionPixel;
-    var tuileXFloor=Math.floor(tuilePositionX);
-    var tuileYFloor=Math.floor(tuilePositionY);
-    var numTuile=tuileYFloor*nbTileWidth+tuileXFloor;
-    console.log(numTuile);
-    return numTuile;
+    }
+    requestAnimationFrame(drawGame);
 }
+
 
 function loadMap(){
     var file = document.getElementById("mapFile").files[0];
@@ -83,23 +54,15 @@ function loadMap(){
     }
 }
 
-function creerMap(){
-    tileDimensionPixel=document.getElementById("tileDimensionPixel").value;
-    nbTileWidth=document.getElementById("nbTileWidth").value;
-    nbTileHeight=document.getElementById("nbTileHeight").value;
-    ctx=document.getElementById('game').getContext('2d');
-    ctx.canvas.width=nbTileWidth*tileDimensionPixel;
-    ctx.canvas.height=nbTileHeight*tileDimensionPixel;
-    for(var y=0; y<nbTileHeight;y++){
-        for(var x=0;x<nbTileWidth;x++){
-            gameMap[(y*nbTileWidth)+x]=1;
-        }
-
-    }
-    requestAnimationFrame(drawGame);
+function scanMap(tx, ty){
+    var tuilePositionX=(tx-5)/tileDimensionPixel;
+    var tuilePositionY=ty/tileDimensionPixel;
+    var tuileXFloor=Math.floor(tuilePositionX);
+    var tuileYFloor=Math.floor(tuilePositionY);
+    var numTuile=tuileYFloor*nbTileWidth+tuileXFloor;
+    console.log(numTuile);
+    return numTuile;
 }
-
-
 
 function drawGame(){
     if(ctx==null){return;}
@@ -130,7 +93,7 @@ function drawGame(){
                 ctx.fillStyle="#ff33d3";
                 ctx.strokeStyle="#000000";
                 break;
-            case 6://objets
+            case 6://hero 4
                 ctx.fillStyle="#823528";
                 ctx.strokeStyle="#000000";
                 break;
@@ -142,4 +105,97 @@ function drawGame(){
         }
     }
     ctx.fillStyle="#ff0000";
+}
+
+window.addEventListener("load", ()=>{
+    canvas= document.getElementById("game");
+    ctx=canvas.getContext("2d");
+    canvas.addEventListener("mousedown", startPositionChangementTextureMap);
+    canvas.addEventListener("mouseup", endPositionChangementTextureMap);
+    canvas.addEventListener("mousemove", draw);
+    document.addEventListener("mousedown", startChangemenfPasEncoreDansCanvas);
+    document.addEventListener("mouseup", endChangemenfPasEncoreDansCanvas);
+    btnWall=document.getElementById("wall");
+    btnGoblin=document.getElementById("goblin");
+    var nomWall=document.getElementById("wall").name;
+    btnWall.addEventListener("click", setChoix(nomWall));
+    var nomGoblin=document.getElementById("goblin").name;
+    btnWall.addEventListener("click", setChoix(nomGoblin));
+    
+});
+
+function startPositionChangementTextureMap(e){
+    changementTextureMap=true;
+    //serait supposer capable de dessiner a un seul clic mais ne fonctionne pas
+    draw(e);
+}
+function endPositionChangementTextureMap(){
+    changementTextureMap=false;
+    ctx.beginPath();
+}
+function startChangemenfPasEncoreDansCanvas(){
+    changementTextureMap=true;
+}
+function endChangemenfPasEncoreDansCanvas(){
+    changementTextureMap=false;
+}
+function tuileSelectionne(e){
+    if(!changementTextureMap){
+        return;
+    }
+}
+function draw(e){
+    if(!changementTextureMap){
+        return;
+    }
+    //holy grail pour Canvas
+    var BB=canvas.getBoundingClientRect();
+    var tx=e.clientX-BB.left;
+    var ty=e.clientY-BB.top;
+    //fin du holy grail
+    numTuile=scanMap(tx,ty);
+    gameMap[numTuile]=couleur;
+    drawGame();
+}
+
+function setChoix(choix){
+    switch(choix){
+    case 0://mur
+        couleur=0;
+        ctx.strokeStyle="#29fdff";
+        break;
+    case 1://marchable
+        couleur=1;
+        ctx.strokeStyle="#29fdff";
+        break;
+    case 2://goblin
+        couleur=2;
+        ctx.strokeStyle="#29fdff";
+        break;
+    case 3://hero 1
+        couleur=3;
+        ctx.strokeStyle="#29fdff";
+        break;
+    case 4://hero 2
+        couleur=4;
+        ctx.strokeStyle="#29fdff";
+        break;
+    case 5://hero 3
+        couleur=5;
+        ctx.strokeStyle="#29fdff";
+        break;
+    case 6://hero 4
+        couleur=6;
+        ctx.strokeStyle="#29fdff";
+        break;
+    }
+}
+function openNav() {
+    document.getElementById("mySidebar").style.width = "500px";
+    document.getElementById("pourSidebar").style.marginRight = "500px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("pourSidebar").style.marginRight= "0";
 }
